c37602580d914538a80ccea90226cd12d869a0af
Antonio <cuccu.a@gmail.com>
2020-02-24 11:26:48 -0500

    register validations completed

--- validation/register.js
+++ validation/register.js
@@ -1,6 +1,7 @@
 const Validator = require('validator');
 const validText = require('./valid-text');
 
+
 module.exports = function validateRegisterInput(data) {
     let errors = {};
 
@@ -9,6 +10,7 @@ module.exports = function validateRegisterInput(data) {
     data.email = validText(data.email) ? data.email : '';
     data.password = validText(data.password) ? data.password : '';
     data.password2 = validText(data.password2) ? data.password2 : '';
+    data.mobile = validText(data.mobile) ? data.mobile : '';
     data.schoolId = typeof data.schoolId === 'integer' ? data.schoolId : ''?;
 
     if (!Validator.isLength(data.firstName, { min: 3, max: 20})) {
@@ -24,8 +26,52 @@ module.exports = function validateRegisterInput(data) {
       };
 
       if (Validator.isEmpty(data.lastName)) {
-        errors.lastName = 'Lasr name is require';
+        errors.lastName = 'Last name is require';
+      };
+
+      if (Validator.isEmpty(data.email)) {
+        errors.email = 'Email field is required';
       }
+
+      if (!Validator.isEmail(data.email)) {
+        errors.email = 'Email is invalid';
+      }
+
+      if (Validator.isEmpty(data.password)) {
+        errors.password = 'Password field is required';
+      }
+    
+      if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+        errors.password = 'Password must be at least 6 characters';
+      }
+
+      if (Validator.isEmpty(data.password2)) {
+        errors.password2 = 'Confirm Password field is required';
+      }
+    
+      if (!Validator.equals(data.password, data.password2)) {
+        errors.password2 = 'Passwords must match';
+      }
+
+      if (Validator.isEmpty(data.mobile)) {
+        errors.mobile = 'Mobile number field is required';
+      }
+
+      if (!Validator.isMobilePhone(data.mobile)) {
+        errors.mobile = 'Mobile is invalid';
+      }
+
+      if (Validator.isEmpty(data.schoolId)) {
+        errors.schoolId = 'School ID field is required';
+      }
+
+      return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+      };
+    };
+
+
 
 
     
