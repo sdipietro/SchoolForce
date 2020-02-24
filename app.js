7427eef97424d047001c7c7816f952d33375cad2
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-02-24 09:31:02 -0500

    starting skeleton

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000;
+
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+// //basic route to render info on our page
+app.get("/", (req, res) => res.send("SchoolForce is in session"));
+
+// app.use("/api/users", users);
+// app.use("/api/tweets", tweets);
+
+// const bodyParser = require('body-parser');
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+//tell Express to start a socket and listen for connections on the path.
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+
+
