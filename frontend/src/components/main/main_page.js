bfccca7c0b85253a48ef87c4e36d162549ec354b
Stephen DiPietro <stevedipietro97@gmail.com>
2020-02-24 15:01:40 -0500

    Finished frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to SchoolForce</p>
+        <footer>Copyright &copy; 2020 SchoolForce</footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
