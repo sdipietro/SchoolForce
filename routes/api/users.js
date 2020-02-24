833ea9ab730bdb1404d9bc57ff00e1517a894066
Antonio <cuccu.a@gmail.com>
2020-02-24 14:48:44 -0500

    passport done

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
+
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+    res.json({
+      id: req.user.id,
+      handle: req.user.handle,
+      email: req.user.email
+    });
+  })
 
 router.post('/register', (req, res) => {
     const { errors, isValid } = validateRegisterInput(req.body);
@@ -95,6 +106,5 @@ router.post('/login', (req, res) => {
     })
 })
 
-router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 module.exports = router;
