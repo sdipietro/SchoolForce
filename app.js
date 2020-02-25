da56e2366bebcb84bdfde68489cdbf543027da7d
Danny Yoo <dansteryoo@gmail.com>
2020-02-25 09:23:47 -0500

    resolving conflict in app.js

--- app.js
+++ app.js
@@ -9,24 +9,28 @@ const reminders = require("./routes/api/reminders");
 const passport = require('passport');
 const port = process.env.PORT || 5000;
 
+// mongoose
+//     .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => console.log("Connected to MongoDB successfully"))
+//     .catch(err => console.log(err));
 mongoose
-    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
 // basic route to render info on our page
-app.get("/", (req, res) => res.send("SchoolForce is in session"));
+// app.get("/", (req, res) => res.send("SchoolForce is in session"));
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
+require('./models/User');
 
 
-// app.use("/api/users", users);
+app.use("/api/user", users);
 // app.use("/api/tweets", tweets);
 
-// const bodyParser = require('body-parser');
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //tell Express to start a socket and listen for connections on the path.
 app.listen(port, () => console.log(`Server is running on port ${port}`));
