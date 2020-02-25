db50786bb54ab9d1088ae73a68bf3cc80aa86f2e
Jesse <jesse.m.colligan@gmail.com>
2020-02-25 16:26:22 -0500

    removing keys_dev.js from master, to not expose keys

--- config/keys_dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://school_force_dev:9Yq3G6TNrHG3gW5e@cluster0-s4v7x.mongodb.net/test?retryWrites=true&w=majority",
-    accountSid: "AC642e51a4c6de0d44e98cd433d5b22e89",
-    authToken: "7f601dc33515ef0cc8a81ca66ce7f8f1",
-    secretOrKey: "VkuJBc6o3BEo8iEaCLvX2B1E37uv99Rx"
-}
