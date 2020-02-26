24775988de436e4a9c3e791ed3a7457e06c9da21
sdipietro <52806794+sdipietro@users.noreply.github.com>
2020-02-26 13:12:16 -0500

    Update signup_form.jsx

--- frontend/src/components/session/signup_form.jsx
+++ frontend/src/components/session/signup_form.jsx
@@ -10,7 +10,7 @@ class SignupForm extends React.Component {
       email: "",
       firstName: "",
       lastName: "",
-      phoneNumber: "",
+      mobile: "",
       password: "",
       password2: "",
       errors: {}
@@ -41,7 +41,7 @@ class SignupForm extends React.Component {
       email: this.state.email,
       firstName: this.state.firstName,
       lastName: this.state.lastName,
-      phoneNumber: this.state.phoneNumber,
+      mobile: this.state.mobile,
       password: this.state.password,
       password2: this.state.password2
     };
