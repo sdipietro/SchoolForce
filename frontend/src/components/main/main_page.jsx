70d85e061787fa475addeaa5f7a3d833ebb0a19f
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-02-26 08:42:46 -0500

    style main page when not logged in, footer, and comment out sign up validations

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import "./main_page.css";
 
 class MainPage extends React.Component {
   render() {
     return (
-      <div>
+      <div id="main-page">
         <header className="login-page-header">
           <div className="login-page-header-left">
             <Link to="/">
@@ -12,12 +13,35 @@ class MainPage extends React.Component {
             </Link>
           </div>
           <div className="main-page-links">
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Signup</Link>
+            <Link className="login-button" to="/login">Login</Link>
+            <Link className="signup-button" to="/signup">Signup</Link>
           </div>
         </header>
-        <p>Welcome to SchoolForce</p>
-        <footer>Copyright &copy; 2020 SchoolForce</footer>
+
+        <section className="main-page-body">
+          <h2 className="main-page-body-main-hook">You're a school director and your students' parents never read your emails?</h2>
+          <img className="main-page-body-frustration-image" src="https://images.unsplash.com/photo-1552345386-6690de5b2c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"/>
+          <div className="main-page-body-descriptions">
+            <div className="main-page-body-second-third-hooks">
+              <h3 className="main-page-body-secondary-hook">SchoolForce is a light CRM for SMS communication with parents 100% from your computer
+                <img className="main-page-body-calm-computer-img" src="https://images.unsplash.com/photo-1501504905252-473c47e087f8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1567&q=80"/>
+              </h3>
+              <h3 className="main-page-body-third-hook">Meet parents where they are - send short reminders that actually get read
+                <img className="main-page-body-happy-parent-img" src="https://images.unsplash.com/photo-1528475775637-ed767f76e6b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
+              </h3>
+              
+            </div>
+            
+            <div className="main-body-page-student-focus-section">
+              <h3 className="main-page-body-fourth-hook">So you get to focus on what matters most: your students</h3>
+              <img className="child-picture" src="https://images.unsplash.com/photo-1580968895877-a19ec54aadc1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"/>
+            </div>
+          </div>
+        </section>
+        
+        <footer className="main-page-footer">
+          <div> Copyright &copy; 2020 SchoolForce</div>
+        </footer>
       </div>
     );
   }
--- validation/register.js
+++ validation/register.js
@@ -17,45 +17,45 @@ module.exports = function validateRegisterInput(data) {
         errors.firstName = 'Please enter your real name';
       };
 
-      if (Validator.isEmpty(data.firstName)) {
-        errors.firstName = 'First name field is require';
-      }
+      // if (Validator.isEmpty(data.firstName)) {
+      //   errors.firstName = 'First name field is require';
+      // }
 
-      if (!Validator.isLength(data.lastName, { min: 3, max: 20 })) {
-        errors.lastName = 'Please enter your real name';
-      };
+      // if (!Validator.isLength(data.lastName, { min: 3, max: 20 })) {
+      //   errors.lastName = 'Please enter your real name';
+      // };
 
-      if (Validator.isEmpty(data.lastName)) {
-        errors.lastName = 'Last name is require';
-      };
+      // if (Validator.isEmpty(data.lastName)) {
+      //   errors.lastName = 'Last name is require';
+      // };
 
-      if (Validator.isEmpty(data.email)) {
-        errors.email = 'Email field is required';
-      }
+      // if (Validator.isEmpty(data.email)) {
+      //   errors.email = 'Email field is required';
+      // }
 
-      if (!Validator.isEmail(data.email)) {
-        errors.email = 'Email is invalid';
-      }
+      // if (!Validator.isEmail(data.email)) {
+      //   errors.email = 'Email is invalid';
+      // }
 
-      if (Validator.isEmpty(data.password)) {
-        errors.password = 'Password field is required';
-      }
+      // if (Validator.isEmpty(data.password)) {
+      //   errors.password = 'Password field is required';
+      // }
     
-      if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
-        errors.password = 'Password must be at least 6 characters';
-      }
+      // if (!Validator.isLength(data.password, { min: 6, max: 30 })) {
+      //   errors.password = 'Password must be at least 6 characters';
+      // }
 
-      if (Validator.isEmpty(data.password2)) {
-        errors.password2 = 'Confirm Password field is required';
-      }
+      // if (Validator.isEmpty(data.password2)) {
+      //   errors.password2 = 'Confirm Password field is required';
+      // }
     
-      if (!Validator.equals(data.password, data.password2)) {
-        errors.password2 = 'Passwords must match';
-      }
+      // if (!Validator.equals(data.password, data.password2)) {
+      //   errors.password2 = 'Passwords must match';
+      // }
 
-      if (Validator.isEmpty(data.mobile)) {
-        errors.mobile = 'Mobile number field is required';
-      }
+      // if (Validator.isEmpty(data.mobile)) {
+      //   errors.mobile = 'Mobile number field is required';
+      // }
 
       // if (!Validator.isMobilePhone(data.mobile)) {
       //   errors.mobile = 'Mobile is invalid';
