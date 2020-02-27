59855169196cfc5ffefab57926c9409e55090cc6
Stephen DiPietro <stevedipietro97@gmail.com>
2020-02-27 12:18:10 -0500

    Fix main page bugs

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -1,8 +1,6 @@
 import {
   RECEIVE_ALL_USERS
-//   RECEIVE_USER
 } from "../actions/user_actions";
-// import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const UsersReducer = (state = { users: {} }, action) => {
   Object.freeze(state);
@@ -15,22 +13,5 @@ const UsersReducer = (state = { users: {} }, action) => {
       return state;
   }
 };
-
-// const UsersReducer = (state = {}, action) => {
-//     debugger
-//   Object.freeze(state);
-//   switch (action.type) {
-//     case RECEIVE_CURRENT_USER:
-//       return Object.assign({}, state, {
-//         [action.currentUser.id]: action.currentUser
-//       });
-//     case RECEIVE_ALL_USERS:
-//       return action.users;
-//     case RECEIVE_USER:
-//       return Object.assign({}, state, { [action.user.id]: action.user });
-//     default:
-//       return state;
-//   }
-// };
 
 export default UsersReducer;
