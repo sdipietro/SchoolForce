fb3170a8968bfad7b6648d7ad299351508980965
Danny Yoo <dansteryoo@gmail.com>
2020-02-27 08:53:52 -0500

    commit ignore

--- frontend/src/components/main/main_page.jsx
+++ frontend/src/components/main/main_page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./main_page.css";
+import { Link } from "react-router-dom";
 
 
 const MainPage = ({ currentUser, logout }) => {
--- frontend/src/util/sms_util.js
+++ frontend/src/util/sms_util.js
@@ -8,12 +8,54 @@ const authToken = require('../../../config/keys').authToken;
 const client = require('twilio')(accountSid, authToken);
 
 // if you just run `node sms_util.js` in the terminal the message below is created and sent:
+
+
+//creating sms
 client.messages
     .create({
         body: 'TESTING Twilio API from Jesse, if you received this slack me! - Schoolforce remninder to bring your own food to the school dinner',
         from: '+14156530533', //this is the Schoolforce Twilio trial number
         to: '+19175793267' //this is Jesse's number
     })
+
+
+//fetching ONE sms from history
+client.messages('MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    .fetch()
+    .then(message => console.log(message.to));
+
+
+//fetching ALL sms from history
+client.messages.list({ limit: 20 })
+    .then(messages => messages.forEach(m => console.log(m.sid)));
+
+
+//fetching SPECIFIC sms from history
+client.messages
+    .list({
+        dateSent: new Date(Date.UTC(2016, 7, 31, 0, 0, 0)),
+        from: '+15017122661',
+        to: '+15558675310',
+        limit: 20
+    })
+    .then(messages => messages.forEach(m => console.log(m.sid)));
+
+
+//fetching SPECIFIC sms within a TIMEFRAME
+client.messages
+    .list({
+        dateSentAfter: new Date(Date.UTC(2019, 0, 1, 0, 0, 0)),
+        dateSentBefore: new Date(Date.UTC(2019, 2, 1, 0, 0, 0)),
+        limit: 20
+    })
+    .then(messages => messages.forEach(m => console.log(m.sid)));
+
+
+//deleting sms
+client.messages('MMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+    .remove();
+
+
 
 // export const sendSMSOneRecipient = (senderNumber, recipientNumber, reminder) => {
 //     client.messages 
