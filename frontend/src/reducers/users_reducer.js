e62c70e373886b8f5065678ff52bfe44600839d5
Jesse <jesse.m.colligan@gmail.com>
2020-02-27 18:11:56 -0500

    update users reducer to be empty object when users slice of state empty

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,7 +2,7 @@ import {
   RECEIVE_ALL_USERS
 } from "../actions/user_actions";
 
-const UsersReducer = (state = {}, action) => {
+const UsersReducer = (state = {users:{}}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_ALL_USERS:
